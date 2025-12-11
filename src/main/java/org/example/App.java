adae46ca4f79a9f548c12d8d671893ae487d5baa
Thangavel8426 <pthangavelpthangavel29@gmail.com>
2025-12-11 15:28:44 +0530

    lombok which remove gettern and setter in the class

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,23 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    Student s1=new Student();
+    s1.setAge(45);
+    s1.setRoll(10);
+        System.out.println(s1.getAge());
+        System.out.println(s1.getRoll());
+
+
+
+        System.out.println( "Hello World!" );
+
+
+    }
+}
